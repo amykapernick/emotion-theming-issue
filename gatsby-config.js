379d78972e16628a9a645b365eef7462892565fd
Amy Kapernick <amy@kapers.dev>
2020-06-03 16:34:16 +0800

    making inline styles visible

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,18 +2,6 @@ module.exports = {
 	plugins: [
 		`gatsby-plugin-sharp`,
 		`gatsby-transformer-sharp`,
-		`gatsby-plugin-react-helmet`,
-		{
-			resolve: `gatsby-plugin-prefetch-google-fonts`,
-			options: {
-				fonts: [
-					{
-						family: `Source Sans Pro`,
-						variants: [`300`, `400`, `600`, `700`]
-					}
-				]
-			}
-		},
 		{
 			resolve: `gatsby-plugin-emotion`,
 			options: {
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { ThemeProvider } from 'emotion-theming';
+import { Global } from '@emotion/core';
 
 import Header from '../header';
 
@@ -11,6 +12,19 @@ const Layout = ({
 }) => (
 	<ThemeProvider theme={altTheme || theme}>
 		<Fragment>
+			<Global
+				styles={{
+					head: {
+						display: `block !important`,
+					},
+					'head style': {
+						display: `block !important`,
+						fontFamily: `monospace`,
+						width: `80vw`,
+						padding: `20px 40px`
+					}
+				}}
+			/>
 			<Header />
 			<Main>{children}</Main>
 			<h2>Theme Prop</h2>
